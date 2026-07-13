6d3331acaf14aac3ae5eb7eb782f2c5bb7f3faaa
Ogunmodede Joel Taiwo <jotelfootball@gmail.com>
2026-07-13 21:42:52 +0100

    chore: remove debug scripts

--- test-balance3.js
+++
@@ -1,4 +0,0 @@
-import { getWalletBalances } from './src/lib/quizaContract.js';
-getWalletBalances(null, '0x31846b45a0b77A4bc1b54aA6f31613A884707Bb7'.toLowerCase(), 'alfajores')
-  .then(console.log)
-  .catch(console.error);
